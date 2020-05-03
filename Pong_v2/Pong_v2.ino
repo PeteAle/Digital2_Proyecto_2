8813f3925b6326bd1cd6915932904951ab8a61df
Pete <yau17914@uvg.edu.gt>
2020-05-02 19:20:27 -0600

    Progra inicial de la v2 del juego.
    Se inició la segunda versión del juego (la primera no se encuentra en este repositorio).

---
+++ Pong_v2/Pong_v2.ino
@@ -0,0 +1,31 @@
+/*Juego de Pong, con funciones adicionales.
+Autores: Daniel Cano, 17272; Peter Yau 17914
+Componentes: 
+  Pantalla de inicio
+  Selección de # de jugadores.
+  Tipo de juego.
+    - Pong normal.
+    - Pong acelerado.
+    - Pong con bola que desaparece.*/
+
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+//#include "Adafruit_ILI9341.h"
+#include "TFT_FastPin.h"
+#include "TFT_ILI9341.h"
+#include "User_Setup.h"
+
+// Pines de DC y CS defaults.
+#define TFT_DC 9
+#define TFT_CS 10
+#define TFT_RST 8
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
